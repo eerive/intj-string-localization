a7df466ac08d9ad6b23740af43e4f77832831aa8
GitHub Action <action@github.com>
2021-08-26 20:40:43 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/eerive/intjstringlocalization/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.eerive.intjstringlocalization.services
+
+import com.github.eerive.intjstringlocalization.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
